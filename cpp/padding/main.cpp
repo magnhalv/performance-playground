f21525b9c7970a3c9232a3bba2a03fa2e1bbcea7
Magnus Halvorsen <magnus.halvorsen@kdi.kongsberg.com>
2024-09-16 08:55:08 +0200

    Refined tests

--- cpp/padding/main.cpp
+++ cpp/padding/main.cpp
@@ -6,75 +6,62 @@
 #include <random>
 #include <string>
 #include <vector>
+#include <sstream>
+#include <iomanip>
+#include <stdlib.h>
 
-typedef int32_t arr_t;
+#include "util.h"
+
+typedef uint32_t arr_t;
+
 
 struct Entry
 {
     arr_t value;
-    arr_t padding[255];
+    arr_t padding[1023];
 };
 
-int64_t total = 0;
-
-auto fill_array(std::vector<Entry> &entries, int size)
+auto fill_array(Entry *entries, int N) -> std::chrono::microseconds
 {
-    entries.resize(size);
-
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<> distr(1, 10);
-
-    for (auto &entry : entries)
+    auto start = std::chrono::high_resolution_clock::now();
+    const int size = N;
+    for (auto i = 0; i < size; i++)
     {
-        entry.value = distr(gen);
+        entries[i].value = i;
     }
+    auto end = std::chrono::high_resolution_clock::now();
+    return std::chrono::duration_cast<std::chrono::microseconds>(end - start);
 }
 
-auto add_together(size_t size, int num_times) -> std::chrono::microseconds
+uint64_t result = 0;
+
+auto sum(const Entry *arr, int N) -> std::chrono::microseconds
 {
-    std::chrono::microseconds duration(0);
-    std::vector<Entry> arr1;
-    std::vector<Entry> arr2;
 
-    fill_array(arr1, size);
-    fill_array(arr2, size);
-
-    for (auto num = 0; num < num_times + 1; num++)
+    auto start = std::chrono::high_resolution_clock::now();
+    const int size = N;
+    for (auto i = 0; i < size; i++)
     {
-        arr_t result = 0;
-
-        auto start = std::chrono::high_resolution_clock::now();
-        for (auto i = 0; i < size; i++)
-        {
-            result += (arr1[i].value + arr2[i].value);
-        }
-
-        auto end = std::chrono::high_resolution_clock::now();
-        if (num != 0)
-        {
-            duration += std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-        }
-        total += result;
+        const auto value = arr[i].value;
+        result += value;
     }
-    return duration / num_times;
+    auto end = std::chrono::high_resolution_clock::now();
+
+    return std::chrono::duration_cast<std::chrono::microseconds>(end - start);
 }
 
 int main(int argc, char *argv[])
 {
-    std::string type = "naive";
-    if (argc == 2)
-    {
-        type = std::string(argv[1]);
-    }
+    auto N = to_mb(12) / sizeof(arr_t);
+    std::cout << "Entry size: " << sizeof(Entry) << " bytes" << std::endl;
+    std::cout << "N = " << N << std::endl;
+    std::cout << "Total array size: " << format_bytes(N * sizeof(Entry)) << std::endl;
 
-    std::cout << "Running with entry size: " << sizeof(Entry) << std::endl;
+    Entry* arr = static_cast<Entry*>(malloc(N * sizeof(Entry)));
 
-    for (auto n = 1; n < 12; n++)
-    {
-        auto N = 30700 * n;
-        auto duration = add_together(N, 30);
-        std::cout << "  N = " << N << ": " << duration.count() << " microseconds." << std::endl;
-    }
-    std::cout << "Total " << total << std::endl;
+    auto duration = fill_array(arr, N);
+    std::cout << "Fill (cold): "<< format_time(duration.count()) << std::endl;
+    duration = sum(arr, N);
+    std::cout << "Sum (warm): "<< format_time(duration.count()) << std::endl;
+    std::cout << result << std::endl;
 }
